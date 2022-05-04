963687bb2a16cbdd69600217437c1a3b6484b487
tassyirifiyya <rifiyya20@mhs.unsyiah.ac.id>
2022-05-04 11:46:42 +0700

    Auto stash before merge of "2008107010040" and "origin/main"

--- resources/views/login_dokter.blade.php
+++ resources/views/login_dokter.blade.php
@@ -47,10 +47,10 @@
                 <a href="{{ URL('/sisi/dokter') }}" class="btn btn-primary">Masuk</a>
             </div>
             <div class="button1"> <br>
-                <a href="{{ URL('/laboran/login') }}" class="btn btn-success" role="button">Masuk sebagai laboran</a>
+                <a href="{{ URL('laboran/login') }}" class="btn btn-success" role="button">Masuk sebagai laboran</a><br>
             </div> <br>
             <div class="button2">
-                <a href="{{ URL('/login/pasien') }}" class="btn btn-dark" role="button">Masuk sebagai pasien</a>
+                <a href="{{ URL('/login/pasien') }}" class="btn btn-dark" role="button">Masuk sebagai pasien</button>
             </div>
         </form>
     </section>
